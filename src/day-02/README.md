4681cf9e9152b837f7a81d3d0945a79fc575d6dc
krsntn <kanahan0110@gmail.com>
2022-01-17 12:00:50 +0800

    day 2

---
+++ src/day-02/README.md
@@ -0,0 +1,5 @@
+# Day 2 - Dive!
+
+## References
+
+https://adventofcode.com/2021/day/2
---
+++ src/day-02/puzzle-01.js
@@ -0,0 +1,26 @@
+const fs = require('fs');
+const filename = 'src/day-02/input.txt';
+const inputList = fs.readFileSync(filename, 'utf8').split('\n');
+
+let position = [0, 0];
+
+inputList.forEach((value) => {
+  let [command, val] = value.split(' ');
+  val = Number(val);
+  switch (command) {
+    case 'forward':
+      position[0] += val;
+      break;
+    case 'up':
+      position[1] -= val;
+      break;
+    case 'down':
+      position[1] += val;
+      break;
+    default:
+      break;
+  }
+});
+
+// output answer
+console.log(position[0] * position[1]);
