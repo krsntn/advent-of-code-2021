5db0cd9bf16b2d92266dc35e045129f0af34bf32
krsntn <kanahan0110@gmail.com>
2022-01-22 22:04:42 +0800

    day 11

---
+++ day-11/README.md
@@ -0,0 +1,5 @@
+# Day 11 - Dumbo Octopus
+
+## References
+
+https://adventofcode.com/2021/day/11
---
+++ day-11/puzzle-01.js
@@ -0,0 +1,135 @@
+const fs = require('fs');
+const filename = 'day-11/input.txt';
+let inputList = fs.readFileSync(filename, 'utf8').split('\n');
+
+let map = inputList.map((line) => line.split('').map(Number));
+
+const step = 100;
+
+function checkFlash([x, y], map) {
+  if (map[x][y] >= 10) {
+    map[x][y] = 0;
+    visited.push([x, y]);
+    addSurround([x, y], map);
+    flashCount++;
+  }
+}
+
+function addSurround([x, y], map) {
+  // top left
+  let newX = x - 1;
+  let newY = y - 1;
+  if (
+    newX >= 0 &&
+    newY >= 0 &&
+    !visited.some(([row, column]) => row === newX && column === newY)
+  ) {
+    map[newX][newY] += 1;
+    checkFlash([newX, newY], map);
+  }
+
+  // top
+  newX = x - 1;
+  newY = y;
+  if (
+    newX >= 0 &&
+    !visited.some(([row, column]) => row === newX && column === newY)
+  ) {
+    map[newX][newY] += 1;
+    checkFlash([newX, newY], map);
+  }
+
+  // top right
+  newX = x - 1;
+  newY = y + 1;
+  if (
+    newX >= 0 &&
+    newY < map[0].length &&
+    !visited.some(([row, column]) => row === newX && column === newY)
+  ) {
+    map[newX][newY] += 1;
+    checkFlash([newX, newY], map);
+  }
+
+  // left
+  newX = x;
+  newY = y - 1;
+  if (
+    newY >= 0 &&
+    !visited.some(([row, column]) => row === newX && column === newY)
+  ) {
+    map[newX][newY] += 1;
+    checkFlash([newX, newY], map);
+  }
+
+  // right
+  newX = x;
+  newY = y + 1;
+  if (
+    newY < map[0].length &&
+    !visited.some(([row, column]) => row === newX && column === newY)
+  ) {
+    map[newX][newY] += 1;
+    checkFlash([newX, newY], map);
+  }
+
+  // bottom left
+  newX = x + 1;
+  newY = y - 1;
+  if (
+    newX < map.length &&
+    newY >= 0 &&
+    !visited.some(([row, column]) => row === newX && column === newY)
+  ) {
+    map[newX][newY] += 1;
+    checkFlash([newX, newY], map);
+  }
+
+  // bottom
+  newX = x + 1;
+  newY = y;
+  if (
+    newX < map.length &&
+    !visited.some(([row, column]) => row === newX && column === newY)
+  ) {
+    map[newX][newY] += 1;
+    checkFlash([newX, newY], map);
+  }
+
+  // bottom right
+  newX = x + 1;
+  newY = y + 1;
+  if (
+    newX < map.length &&
+    newY < map[0].length &&
+    !visited.some(([row, column]) => row === newX && column === newY)
+  ) {
+    map[newX][newY] += 1;
+    checkFlash([newX, newY], map);
+  }
+}
+
+let visited = [];
+let flashCount = 0;
+
+for (let i = 0; i < step; i++) {
+  visited = [];
+  for (let x = 0; x < map.length; x++) {
+    for (let y = 0; y < map[x].length; y++) {
+      if (visited.some(([row, column]) => row === x && column === y)) continue;
+
+      const newValue = map[x][y] + 1;
+      if (newValue >= 10) {
+        map[x][y] = 0;
+        flashCount++;
+        visited.push([x, y]);
+        addSurround([x, y], map);
+        continue;
+      }
+
+      map[x][y] = newValue;
+    }
+  }
+}
+
+console.log(flashCount);
