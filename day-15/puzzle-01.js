4757b7320d0120f907c003b7f9f95e9fb131afe6
krsntn <kanahan0110@gmail.com>
2022-01-30 03:42:42 +0800

    day 15

--- day-15/puzzle-01.js
+++ day-15/puzzle-01.js
@@ -1,121 +1,69 @@
-// const fs = require("fs");
-// const filename = "day-14/input.txt";
-// const inputList = fs.readFileSync(filename, "utf8").split("\n");
+const fs = require("fs");
+const filename = "day-15/input.txt";
+const inputList = fs
+  .readFileSync(filename, "utf8")
+  .split("\n")
+  .map((x) => [...x].map(Number));
 
-const inputList = `1163751742
-1381373672
-2136511328
-3694931569
-7463417111
-1319128137
-1359912421
-3125421639
-1293138521
-2311944581`.split("\n");
+const rowLength = inputList.length;
+const columnLength = inputList[0].length;
+const endIndex = rowLength * columnLength - 1;
 
-const map = [];
+const totalArrLen = rowLength * columnLength;
+const distanceList = Array(totalArrLen).fill(Infinity);
+const indexes = new Set(
+  Array(totalArrLen)
+    .fill(0)
+    .map((_, index) => index)
+);
 
-for (let i = 0; i < inputList.length; i++) {
-  const line = inputList[i];
+function updateNeighborDistance(nX, nY, rowLength, columnLength, minIndex) {
+  if (nX >= 0 && nX < columnLength && nY >= 0 && nY < rowLength) {
+    const neighborUpValue = inputList[nX][nY];
+    if (neighborUpValue) {
+      const neighborindex = nX + nY * columnLength;
+      const newValue = distanceList[minIndex] + neighborUpValue;
 
-  line.split("").forEach((x, index) => {
-    map.push({
-      value: Number(x),
-      x: i,
-      y: index,
-    });
-  });
+      if (newValue < distanceList[neighborindex]) {
+        distanceList[neighborindex] = newValue;
+      }
+    }
+  }
 }
 
-const visited = [];
-let unvisited = JSON.parse(JSON.stringify(map));
+// set first data
+distanceList[0] = 0;
 
-let currentNode = unvisited.shift();
+while (distanceList[endIndex] === Infinity) {
+  let minDistance = Infinity;
+  let minIndex = 0;
 
-const table = [
-  {
-    vertex: `${currentNode.x},${currentNode.y}`,
-    distance: 0,
-    previousVertex: null,
-  },
-];
-
-while (currentNode) {
-  const { x, y } = currentNode;
-
-  const neighbors = unvisited.filter(
-    (node) =>
-      (node.x === x + 1 && node.y === y) ||
-      (node.x === x - 1 && node.y === y) ||
-      (node.x === x && node.y === y + 1) ||
-      (node.x === x && node.y === y - 1)
-  );
-
-  if (neighbors) {
-    const allDistances = [];
-    neighbors.forEach((neighbor) => {
-      const currentVertex = table.find((z) => z.vertex === `${x},${y}`);
-      const neighborVertex = table.find(
-        (z) => z.vertex === `${neighbor.x},${neighbor.y}`
-      );
-      const totalDistance = neighbor.value + currentVertex.distance;
-
-      if (neighborVertex) {
-        if (neighborVertex.distance > totalDistance) {
-          neighborVertex.distance = totalDistance;
-          allDistances.push(totalDistance);
-        } else {
-          allDistances.push(neighborVertex.distance);
-        }
-      } else {
-        table.push({
-          vertex: `${neighbor.x},${neighbor.y}`,
-          distance: totalDistance,
-          previousVertex: `${currentNode.x},${currentNode.y}`,
-        });
-        allDistances.push(totalDistance);
-      }
-    });
-
-    const availableData = table.filter((data) =>
-      unvisited.some(
-        (node) =>
-          node.x == data.vertex.split(",")[0] &&
-          node.y == data.vertex.split(",")[1]
-      )
-    );
-
-    const smallestNeighbor =
-      neighbors[allDistances.indexOf(Math.min(...allDistances))];
-
-    // const min = Math.min(...neighbors.map((x) => x.value));
-    // const smallestNeighbor = neighbors.find((x) => x.value === min);
-    if (smallestNeighbor) {
-      currentNode = smallestNeighbor;
-      // } else if (
-      //   !visited.find(
-      //     (node) =>
-      //       node.x === inputList.length - 1 && node.y === inputList[0].length - 1
-      //   )
-      // ) {
-      //   console.log("what", currentNode);
-      //   currentNode = unvisited[0];
-    } else {
-      if (
-        visited.some(
-          (node) =>
-            node.x === inputList.length - 1 &&
-            node.y === inputList[0].length - 1
-        )
-      ) {
-        break;
-      }
-      currentNode = unvisited[0];
+  // get min node
+  for (const value of indexes) {
+    if (distanceList[value] < minDistance) {
+      minDistance = distanceList[value];
+      minIndex = value;
     }
   }
 
-  visited.push(currentNode);
-  unvisited.splice(unvisited.indexOf(currentNode), 1);
+  // remove from indexes
+  indexes.delete(minIndex);
+
+  // reached to the end point
+  if (minIndex === endIndex) break;
+
+  // find neighbors and update distance
+  const minCoordX = minIndex % columnLength;
+  const minCoordY = (minIndex - minCoordX) / rowLength;
+
+  [
+    [minCoordX - 1, minCoordY],
+    [minCoordX + 1, minCoordY],
+    [minCoordX, minCoordY - 1],
+    [minCoordX, minCoordY + 1],
+  ].map(([x, y]) =>
+    updateNeighborDistance(x, y, rowLength, columnLength, minIndex)
+  );
 }
 
-// console.log(table);
+console.log(distanceList[endIndex]);
--- day-15/puzzle-02.js
+++ day-15/puzzle-02.js
@@ -1,3 +1,3 @@
 const fs = require("fs");
-const filename = "day-14/input.txt";
+const filename = "day-15/input.txt";
 const inputList = fs.readFileSync(filename, "utf8").split("\n");
