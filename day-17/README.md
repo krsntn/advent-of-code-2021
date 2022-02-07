0fc85f203dfd766c702b3509b490ad0ecd0beacf
krsntn <kanahan0110@gmail.com>
2022-02-07 12:05:26 +0800

    day 17

---
+++ day-17/README.md
@@ -0,0 +1,5 @@
+# Day 17 - Trick Shot
+
+## References
+
+https://adventofcode.com/2021/day/17
---
+++ day-17/puzzle-01.js
@@ -0,0 +1,58 @@
+const fs = require("fs");
+const filename = "day-16/input.txt";
+const inputList = fs
+  .readFileSync(filename, "utf8")
+  .replace(/[^0-9. -]/g, "")
+  .trim();
+
+const targetRangeX = inputList.split(" ")[0].split("..").map(Number);
+const targetRangeY = inputList.split(" ")[1].split("..").map(Number);
+
+const withinTargets = [];
+
+for (let i = 0; i < 300; i++) {
+  for (let j = 0; j < 300; j++) {
+    let overTarget = false;
+    let posX = 0;
+    let posY = 0;
+    let highestPosY = 0;
+
+    let x = i;
+    let y = j;
+
+    while (!overTarget) {
+      if (x < 0) {
+        x = 0;
+      }
+
+      posX += x;
+      posY += y;
+
+      if (posY > highestPosY) {
+        highestPosY = posY;
+      }
+
+      // inside target
+      if (
+        posX >= targetRangeX[0] &&
+        posX <= targetRangeX[1] &&
+        posY >= targetRangeY[0] &&
+        posY <= targetRangeY[1]
+      ) {
+        withinTargets.push([i, j, highestPosY]);
+        break;
+      }
+
+      // over target
+      overTarget = posY < targetRangeY[0];
+      if (!overTarget && posX > targetRangeX[1]) {
+        overTarget = true;
+      }
+
+      x--;
+      y--;
+    }
+  }
+}
+
+console.log(withinTargets);
